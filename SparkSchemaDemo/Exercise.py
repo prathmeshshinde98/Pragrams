05e03ee564ffafbd3c7defce34f9bd998aacf5f6
prathmeshshinde98 <prathmeshshinde1998@gmail.com>
2023-08-19 09:39:24 +0530

    files deleted

--- SparkSchemaDemo/Exercise.py
+++ SparkSchemaDemo/Exercise.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import col,lit
 
 if __name__ == '__main__':
     spark = SparkSession.builder.master('local[2]').appName('exercise').getOrCreate()
-    df = spark.read.format('csv').option('header',True).option('schemaName',True).load('Data/ChatGPT.csv')
+    df = spark.read.format('csv').option('header',True).option('schemaName',True).load('Data/Sales_CGPT.csv')
     # Select 5 Rows only
     df2 = df.limit(5)
     # Multiplication of Quantity and PricePerUnit to get TotalSales numbers
@@ -13,10 +13,13 @@ if __name__ == '__main__':
     # Typecast Quantity column to integer to add all the quantity for individual product
     df5 = df3.withColumn("Quantity", col("Quantity").cast("integer"))
     # Adding the Quantity
+    df5.show()
     df6 = df5.groupBy("Product").sum("Quantity")
     # Adding Sum of Quantity Dataframe(df6) to Sum of TotalSales Dataframe(df4)
     df7 = df6.join(df4.select("Product","sum(TotalSales)"),on="Product",how="inner")
     # Rename columns to proper naming
     df8 = df7.withColumnRenamed("sum(Quantity)","Quantity").withColumnRenamed("sum(TotalSales)","TotalRevenue")
+    df9 = df8.sort("Quantity")
     df8.show()
+    df9.show()
     spark.stop()
